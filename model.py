bfe4642279b7da5414e4d5caabbd1c44a30bbca5
d4ni3lb <danielberlien@gmail.com>
2016-08-25 11:01:21 -0300

    Modificado model.py, ahora contiene un par de métodos que manejan datos de la base de datos para ser mostrados

--- model.py
+++ model.py
@@ -4,26 +4,28 @@
 import sqlite3
 
 def conectar():
+#Para conectar a la base de datos
 	con = sqlite3.connect('locales.db')
-    con.row_factory = sqlite3.Row
+    con.row_factory = sqlite3.Row #Propiedad row para defnir que tipo de objeto representa cada fila en los resultados del query, en este caso como Row de sqlite3
     return con
 
 
 def locales():
-	
+#Mostrar en la grilla todos los locales de la bd
     con = conectar()
-    c = con.cursor()
-    query = "SELECT * FROM local"
-    resultado = c.execute(query)
-    locales = resultado.fetchall()
-    con.close()
+    c = con.cursor() #Definimos el cursor de la conexion
+    query = "SELECT * FROM local" #Definimos la query
+    resultado = c.execute(query) #La ejecutamos
+    locales = resultado.fetchall() #Guardamos todos los resultados en "locales"
+    con.close() #Cerrar la conexion
     return locales
 
-def alumno(rut):
+def locales_por_ciudad(n_ciudad):
+#Mostrar en la grilla los locales cuya ciudad coincida con la indicada en el argumento
     con = conectar()
     c = con.cursor()
-    query = "SELECT * FROM alumnos WHERE rut = ?"
-    resultado = c.execute(query, [rut])
-    alumno = resultado.fetchone()
-    con.close()
-    return alumno
+    query = "SELECT nombre_l,ciudad.nombre_c FROM local JOIN ciudad on local.fk_id_ciudad = ciudad.id_ciudad WHERE ciudad.nombre_c LIKE ?"
+    resultado = c.execute(query, n_ciudad) #Ejecutamos la query
+    locales_ciudad = resultado.fetchall() #Guardamos todos los resultados
+    con.close() #Cerramos la conexion
+    return locales_ciudad
