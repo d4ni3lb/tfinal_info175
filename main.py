6c509b9d2b3fcd2e2f077659941dc229b4a73025
d4ni3lb <danielberlien@gmail.com>
2016-08-29 15:33:03 -0300

    Modificado el main para filtrar por ciudades (sin tomar en cuenta el combobox)

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ class Main(QtGui.QWidget):
 		super(Main, self).__init__()
 		self.ui = grid_ui.Ui_Form()
 		self.ui.setupUi(self)
+		self.connect_signals()
 		self.load_grid()
 
+
+	def connect_signals(self):
+		self.ui.buscador.textChanged.connect(self.onChanged)
 
 	def load_grid(self):
 		locales = db_control.locales()
@@ -33,6 +37,34 @@ class Main(QtGui.QWidget):
 			self.data.setData(index, row['Direccion'])
 			index = self.data.index(r, 3, QtCore.QModelIndex())
 			self.data.setData(index, row['Total_empleados'])
+
+	def load_filtered_grid1(self,text):
+		if(text!=""):
+			locales = db_control.locales_por_ciudad(text)
+
+			self.data = QtGui.QStandardItemModel(len(locales), 4)
+			self.data.setHorizontalHeaderItem(0, QtGui.QStandardItem(u"Local"))
+			self.data.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Ciudad"))
+			self.data.setHorizontalHeaderItem(2, QtGui.QStandardItem(u"Dirección"))
+			self.data.setHorizontalHeaderItem(3, QtGui.QStandardItem(u"Total_empleados"))
+			self.ui.tableView.setModel(self.data)
+
+			for r, row in enumerate(locales):
+				index = self.data.index(r, 0, QtCore.QModelIndex())
+				self.data.setData(index, row['Local'])
+				index = self.data.index(r, 1, QtCore.QModelIndex())
+				self.data.setData(index, row['Ciudad'])
+				index = self.data.index(r, 2, QtCore.QModelIndex())
+				self.data.setData(index, row['Direccion'])
+				index = self.data.index(r, 3, QtCore.QModelIndex())
+				self.data.setData(index, row['Total_empleados'])
+		else:
+			self.load_grid()
+
+
+	def onChanged(self,text):
+		self.load_filtered_grid1(text)
+		
 
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
