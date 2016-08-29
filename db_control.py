b3157ef4d153ccda95337c0d397570e6c1217c12
Blacky <juancontre91@gmail.com>
2016-08-29 16:59:20 -0400

    query arreglada

--- db_control.py
+++ db_control.py
@@ -14,7 +14,7 @@ def locales():
 #Mostrar en la grilla todos los locales de la bd
     con = conectar()
     c = con.cursor() #Definimos el cursor de la conexion
-    query = "SELECT nombre_l AS Local ,ciudad.nombre_c AS Ciudad ,local.direccion, COUNT(nombre) AS Total_empleados FROM local,ciudad LEFT join  empleado on local.fk_id_ciudad = ciudad.id_ciudad and empleado.fk_id_local = local.id_local GROUP BY nombre_l" #Definimos la query
+    query = "SELECT nombre_l AS Local ,ciudad.nombre_c AS Ciudad ,local.direccion, COUNT(nombre) AS Total_empleados FROM local,ciudad LEFT join  empleado on local.fk_id_ciudad = ciudad.id_ciudad and empleado.fk_id_local = local.id_local GROUP BY nombre_l;" #Definimos la query
     resultado = c.execute(query) #La ejecutamos
     locales = resultado.fetchall() #Guardamos todos los resultados en "locales"
     con.close() #Cerrar la conexion
