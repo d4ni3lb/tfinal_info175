9cceb266bb8598ab1a6700c52f75fe18f5be25e9
Blacky <juancontre91@gmail.com>
2016-08-29 16:48:15 -0400

    arreglado el condoro

--- main.py
+++ main.py
@@ -7,25 +7,24 @@ import grid_ui
 from PySide import QtGui, QtCore
 
 class Main(QtGui.QWidget):
-	def __init__(self):
-		super(Main, self).__init__()
-		self.ui = grid_ui.Ui_Form()
-		self.ui.setupUi(self)
-		self.connect_signals()
-		self.load_grid()
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = grid_ui.Ui_Form()
+        self.ui.setupUi(self)
+        self.connect_signals()
+        self.load_grid()
 
 
-	def connect_signals(self):
-		self.ui.buscador.textChanged.connect(self.onChanged)
+    def connect_signals(self):
+        self.ui.buscador.textChanged.connect(self.onChanged)
 
-	def load_grid(self):
-		locales = db_control.locales()
-
-		self.data = QtGui.QStandardItemModel(len(locales), 4)
-		self.data.setHorizontalHeaderItem(0, QtGui.QStandardItem(u"Local"))
-		self.data.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Ciudad"))
-		self.data.setHorizontalHeaderItem(2, QtGui.QStandardItem(u"Dirección"))
-		self.data.setHorizontalHeaderItem(3, QtGui.QStandardItem(u"Total_empleados"))
+    def load_grid(self):
+        locales = db_control.locales()
+        self.data = QtGui.QStandardItemModel(len(locales), 4)
+        self.data.setHorizontalHeaderItem(0, QtGui.QStandardItem(u"Local"))
+        self.data.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Ciudad"))
+        self.data.setHorizontalHeaderItem(2, QtGui.QStandardItem(u"Dirección"))
+        self.data.setHorizontalHeaderItem(3, QtGui.QStandardItem(u"Total_empleados"))
         
         for r, row in enumerate(locales):
             index = self.data.index(r, 0, QtCore.QModelIndex())
