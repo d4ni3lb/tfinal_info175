ba21799995dd5bbf7244ce36ccfbb0104a5b6d63
Marco <ravield1192@gmail.com>
2016-08-29 16:07:12 -0300

    probelamas de identacion

--- main.py
+++ main.py
@@ -26,18 +26,19 @@ class Main(QtGui.QWidget):
 		self.data.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Ciudad"))
 		self.data.setHorizontalHeaderItem(2, QtGui.QStandardItem(u"Dirección"))
 		self.data.setHorizontalHeaderItem(3, QtGui.QStandardItem(u"Total_empleados"))
-		self.ui.tableView.setModel(self.data)
-
-		for r, row in enumerate(locales):
-			index = self.data.index(r, 0, QtCore.QModelIndex())
-			self.data.setData(index, row['Local'])
-			index = self.data.index(r, 1, QtCore.QModelIndex())
-			self.data.setData(index, row['Ciudad'])
-			index = self.data.index(r, 2, QtCore.QModelIndex())
-			self.data.setData(index, row['Direccion'])
-			index = self.data.index(r, 3, QtCore.QModelIndex())
-			self.data.setData(index, row['Total_empleados'])
+        
+        for r, row in enumerate(locales):
+            index = self.data.index(r, 0, QtCore.QModelIndex())
+            self.data.setData(index, row['Local'])
+            index = self.data.index(r, 1, QtCore.QModelIndex())
+            self.data.setData(index, row['Ciudad'])
+            index = self.data.index(r, 2, QtCore.QModelIndex())
+            self.data.setData(index, row['Direccion'])
+            index = self.data.index(r, 3, QtCore.QModelIndex())
+            self.data.setData(index, row['Total_empleados'])
             
+        self.ui.tableView.setModel(self.data)
+        
         self.ui.tableView.horizontalHeader().setResizeMode(1, self.ui.tableView.horizontalHeader().Stretch)
         self.ui.tableView.horizontalHeader().setResizeMode(2, self.ui.tableView.horizontalHeader().Stretch)
         
@@ -45,43 +46,42 @@ class Main(QtGui.QWidget):
         self.ui.tableView.setColumnWidth(1, 210)
         self.ui.tableView.setColumnWidth(2, 210)
         self.ui.tableView.setColumnWidth(3, 100)
-
-	def load_filtered_grid1(self,text):
-		if(text!=""):
-			locales = db_control.locales_por_ciudad(text)
-
-			self.data = QtGui.QStandardItemModel(len(locales), 4)
-			self.data.setHorizontalHeaderItem(0, QtGui.QStandardItem(u"Local"))
-			self.data.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Ciudad"))
-			self.data.setHorizontalHeaderItem(2, QtGui.QStandardItem(u"Dirección"))
-			self.data.setHorizontalHeaderItem(3, QtGui.QStandardItem(u"Total_empleados"))
-			self.ui.tableView.setModel(self.data)
-
-			for r, row in enumerate(locales):
-				index = self.data.index(r, 0, QtCore.QModelIndex())
-				self.data.setData(index, row['Local'])
-				index = self.data.index(r, 1, QtCore.QModelIndex())
-				self.data.setData(index, row['Ciudad'])
-				index = self.data.index(r, 2, QtCore.QModelIndex())
-				self.data.setData(index, row['Direccion'])
-				index = self.data.index(r, 3, QtCore.QModelIndex())
-				self.data.setData(index, row['Total_empleados'])
-                
-			self.ui.tableView.horizontalHeader().setResizeMode(1, self.ui.tableView.horizontalHeader().Stretch)
-			self.ui.tableView.horizontalHeader().setResizeMode(2, self.ui.tableView.horizontalHeader().Stretch)
-            
-			self.ui.tableView.setColumnWidth(0, 220)
-			self.ui.tableView.setColumnWidth(1, 210)
-			self.ui.tableView.setColumnWidth(2, 210)
-			self.ui.tableView.setColumnWidth(3, 100)
-            
-		else:
-			self.load_grid()
-            
         
-
-	def onChanged(self,text):
-		self.load_filtered_grid1(text)
+    def load_filtered_grid1(self,text):
+        if(text!=""):
+            locales = db_control.locales_por_ciudad(text)
+            
+            self.data = QtGui.QStandardItemModel(len(locales), 4)
+            self.data.setHorizontalHeaderItem(0, QtGui.QStandardItem(u"Local"))
+            self.data.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Ciudad"))
+            self.data.setHorizontalHeaderItem(2, QtGui.QStandardItem(u"Dirección"))
+            self.data.setHorizontalHeaderItem(3, QtGui.QStandardItem(u"Total_empleados"))
+            
+            for r, row in enumerate(locales):
+                index = self.data.index(r, 0, QtCore.QModelIndex())
+                self.data.setData(index, row['Local'])
+                index = self.data.index(r, 1, QtCore.QModelIndex())
+                self.data.setData(index, row['Ciudad'])
+                index = self.data.index(r, 2, QtCore.QModelIndex())
+                self.data.setData(index, row['Direccion'])
+                index = self.data.index(r, 3, QtCore.QModelIndex())
+                self.data.setData(index, row['Total_empleados'])
+                
+            self.ui.tableView.setModel(self.data)
+            
+            self.ui.tableView.horizontalHeader().setResizeMode(1, self.ui.tableView.horizontalHeader().Stretch)
+            self.ui.tableView.horizontalHeader().setResizeMode(2, self.ui.tableView.horizontalHeader().Stretch)
+            
+            self.ui.tableView.setColumnWidth(0, 220)
+            self.ui.tableView.setColumnWidth(1, 210)
+            self.ui.tableView.setColumnWidth(2, 210)
+            self.ui.tableView.setColumnWidth(3, 100)
+            
+        else:
+            self.load_grid()
+            
+    def onChanged(self,text):
+        self.load_filtered_grid1(text)
 		
 
 if __name__ == "__main__":
