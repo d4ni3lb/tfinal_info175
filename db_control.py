9d36427db3193ff5e95bb576e9dc22d079910db1
Blacky <juancontre91@gmail.com>
2016-09-01 10:56:46 -0400

    Querys arregladas

--- db_control.py
+++ db_control.py
@@ -14,7 +14,7 @@ def locales():
 #Mostrar en la grilla todos los locales de la bd
     con = conectar()
     c = con.cursor() #Definimos el cursor de la conexion
-    query = "SELECT nombre_l AS Local ,ciudad.nombre_c AS Ciudad ,local.direccion, COUNT(nombre) AS Total_empleados FROM local,ciudad LEFT join  empleado on local.fk_id_ciudad = ciudad.id_ciudad and empleado.fk_id_local = local.id_local GROUP BY nombre_l;" #Definimos la query
+    query = "SELECT nombre_l AS Local ,ciudad.nombre_c AS Ciudad ,local.direccion, COUNT(nombre) AS Total_empleados FROM local inner join ciudad ON  local.fk_id_ciudad = ciudad.id_ciudad left join empleado ON  empleado.fk_id_local = local.id_local GROUP BY nombre_l;" #Definimos la query
     resultado = c.execute(query) #La ejecutamos
     locales = resultado.fetchall() #Guardamos todos los resultados en "locales"
     con.close() #Cerrar la conexion
@@ -25,7 +25,7 @@ def locales_por_ciudad(n_ciudad):
 	con = conectar()
 	c = con.cursor()
 	n_ciudad = "%"+n_ciudad+"%"
-	query = "SELECT nombre_l AS Local ,ciudad.nombre_c AS Ciudad ,local.direccion, COUNT(nombre) AS Total_empleados FROM local ,empleado join ciudad  on local.fk_id_ciudad = ciudad.id_ciudad and  empleado.fk_id_local = local.id_local WHERE ciudad.nombre_c LIKE ? GROUP BY nombre_l"
+	query = "SELECT nombre_l AS Local ,ciudad.nombre_c AS Ciudad ,local.direccion, COUNT(nombre) AS Total_empleados FROM local inner join ciudad ON local.fk_id_ciudad = ciudad.id_ciudad left join empleado ON empleado.fk_id_local = local.id_local WHERE ciudad.nombre_c LIKE ? GROUP BY nombre_l;"
 	resultado = c.execute(query, [n_ciudad]) #Ejecutamos la query
 	locales_ciudad = resultado.fetchall() #Guardamos todos los resultados
 	con.close() #Cerramos la conexion
