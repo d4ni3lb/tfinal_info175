8c0d50501923e09d90bd14a8fa21964bc534c116
ravield <ravield1192@gmail.com>
2016-09-04 14:14:10 -0300

    agregadas funciones del formulario

--- abrir_form_local.py
+++ abrir_form_local.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import os
 import sys
 import db_control
 import form_local_ui
 from PySide import QtGui, QtCore
 
-class FormularioLocales(QtGui.QWidget):
-    def __init__(self):
+class FormularioLocales(QtGui.QDialog):
+    reloadT = QtCore.Signal()
+    identificador = False
+    def __init__(self, id=None):
         super(FormularioLocales, self).__init__()
-        self.ui = form_local_ui.Ui_Form()
+        self.ui = form_local_ui.Ui_Dialog()
         self.ui.setupUi(self)
+        self.show()
         self.connect_signals()
+        if(id == None):
+            self.id=0
+            self.identificador=False
+            self.setWindowTitle("Nuevo Local")
+            self.show()
+        else:
+            self.id=id
+            self.identificador = True
+            self.setWindowTitle("Editar Local")
+            local = db_control.obtenerLocalID(id)
+            for row in local:
+                self.ui.lineNombreL.setText(row[u'nombre_l'])
+                self.ui.lineDireccion.setText(row[u'direccion'])
+                self.ui.lineCiudad.setText(row[u'ciudad'])
+
+        self.setMaximumHeight(self.height()-1)
+        self.setMaximumWidth(self.width()-1)
 
     def connect_signals(self):
-        self.ui.pushButton.clicked.connect(self.action_aceptar)
-        self.ui.pushButton_2.clicked.connect(self.action_cancelar)
+        self.accepted.connect(self.action_aceptar)
 
     def action_cancelar(self):
         exit()
